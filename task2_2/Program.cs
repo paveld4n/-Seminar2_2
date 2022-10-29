5d3bea7cfa8b1074a20ace8af84116373658f492
Pavel <rut100500@yandex.ru>
2022-10-29 20:22:40 +0300

    Seminar2 ver.8 краность 7 и 23

---
+++ task2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает число и решает кратно 23 и 7
+int number = 161;
+
+CompareNumder(number);
+void CompareNumder(int num)
+{
+    int result1 = num % 7;
+    int result2 = num % 23;
+    if(result1 ==0 & result2 == 0)
+    {
+        Console.WriteLine("Полученное число кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Полученное число НЕ кратно 7 и 23");
+    }
+}
