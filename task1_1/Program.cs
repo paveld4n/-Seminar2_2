df3ecccea19a1d8baa8d0a0405d1c0a4b88c74c3
Pavel <rut100500@yandex.ru>
2022-10-29 11:27:43 +0300

    Seminar2 ver.2

---
+++ task1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int randomNum = GetRandomNum(100, 100);
+Console.WriteLine(randomNum);
+
+int max = randomNum % 10; /// задается последнее число, корое решено считать максимальным
+randomNum = randomNum / 10; /// переменная уменьшается на 1 разряд
+
+while(randomNum > 0)
+{
+    int lastNum = randomNum % 10;
+    if (lastNum > max)
+    {
+        max = lastNum;
+    }
+    randomNum = randomNum / 10;
+}
+
+int lastNum = randomNum % 10;
+Console.WriteLine(lastNum);
+
+int GetRandomNum(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNum = random.Next(minValue, maxValue);
+    return randomNum;
+}
