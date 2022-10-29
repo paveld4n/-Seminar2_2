eb5b4b95c5fc11604e5f43a9e3de2218bdb42567
Pavel <rut100500@yandex.ru>
2022-10-29 19:04:02 +0300

    Seminar2 ver.7 убратьсреднюю цифру м

---
+++ task2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = GetRandomNumber(100, 1000);
+int result = DeleteSecondDigit(number);
+Console.WriteLine(number);
+Console.WriteLine(result);
+
+int DeleteSecondDigit(int number)
+{
+    int lastNum = number % 10;
+    number = number / 100;
+    int result = number * 10 + lastNum;
+    return result;
+}
+
+int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
