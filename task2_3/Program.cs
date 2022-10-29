10ed555a8c777cc6b79a53fc32cc45ebb452e23c
Pavel <rut100500@yandex.ru>
2022-10-29 22:27:37 +0300

    Seminar2 ver.9 оно квадрат? через bool

---
+++ task2_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает два числа и решает является ли одно квадратом другого
+Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckSquare(num1, num2);
+if(result)
+{
+    Console.WriteLine("Да, квадрат");
+}
+else
+{
+    Console.WriteLine("Нет, не квадрат");
+}
+
+// метод
+bool CheckSquare(int firstNumber, int secondNumber)
+{
+    int square1 = firstNumber * firstNumber;
+    int square2 = secondNumber * secondNumber;
+
+    return square1 == secondNumber | square2 == firstNumber;
+}
